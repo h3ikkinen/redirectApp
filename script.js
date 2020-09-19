61f365b1d12a8d06d91fed338bedd61bef4b30c1
h3ikkinen <60520230+h3ikkinen@users.noreply.github.com>
2020-09-19 18:03:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+
+//     alert("Вы используете мобильное устройство (телефон или планшет).")
+
+// } else alert("Вы используете ПК.")
+if (/Android/i.test(navigator.userAgent)) {
+    // alert('вы используете андроид');
+    window.location.href= "https://play.google.com/store/apps/details?id=com.vkontakte.android&hl=ru";
+    // window.location.href= "market://details?id=<packagename>";
+} else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+    window.location.href = "https://apps.apple.com/ru/app/вконтакте-общение-и-музыка/id564177498";
+}
